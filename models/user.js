ded66432359983a06d0314e5ceaacc873b57308a
Prateek Sharma <prateek.sharma@bankit.in>
2022-06-25 19:29:42 +0530

    done

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    userId :{
+        
+        type: Number
+    },
+
+    emailAddress:{
+        type: String
+    },
+    token: {
+       
+        type: String
+    },
+
+    name :{
+       
+        type: String
+    },
+    
+
+   
+})
+
+module.exports = mongoose.model('user',userSchema)
+
